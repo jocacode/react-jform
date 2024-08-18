335df423b36486899b295d279a7a5d1b7c359f72
aritonovicjovan <Aritonovicjovan@gmail.com>
2024-08-19 01:30:01 +0200

    feat(init): react jform init repo

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  resolve: { alias: { '@src': resolve('./src') } },
+  plugins: [
+    tsConfigPaths({}),
+    dts({
+      insertTypesEntry: true,
+      include: ['*/*.ts'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'react-jstore',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        format: 'es',
+        dir: 'dist/',
+      },
+    },
+  },
+});
